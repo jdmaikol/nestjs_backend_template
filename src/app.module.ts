923f688a89f741de22b56d45adaa14ecd6f7739c
Maikol Moreno <jdmaikol44@gmail.com>
2026-01-02 15:29:18 -0400

    setting db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.TYPEORM_HOST,
+      port: Number(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [],
+      synchronize: Boolean(process.env.TYPEORM_SYNCHRONIZE),
+      autoLoadEntities: Boolean(process.env.TYPEORM_AUTOLOADENTITIES),
+      ssl: Boolean(process.env.TYPEORM_SSL),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
